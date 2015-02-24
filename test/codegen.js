f4b8fd5191aa7f97b707595dd7231db2427a55ca
Mikola Lysenko <mikolalysenko@gmail.com>
2015-02-24 10:21:00 -0600

    working on code generation, tracing and float unpacking

---
+++ test/codegen.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var CASES = require('./cases')
+var parse = require('../parse')
+var codegen = require('../codegen')
+var tape = require('tape')
+
+Object.keys(CASES).forEach(function(filename) {
+  tape('gen ' + filename, function(t) {
+    var src = CASES[filename]
+    var ast = parse(src)
+    
+    console.log(codegen(ast).code)
+
+    t.end()
+  })
+})
