84f2c27fa16e1b50e27dfa56228432ba9300e939
Mikola Lysenko <mikolalysenko@gmail.com>
2015-02-24 16:44:48 -0600

    working on interval arithmetic phase

--- codegen.js
+++ codegen.js
@@ -3,7 +3,7 @@
 module.exports = generateCode
 
 var compileBigInt = require('./lib/bigint-codegen')
-
+var compileInterval = require('./lib/interval-codegen')
 
 //Future:  Use staged predicates:
 //
@@ -17,14 +17,23 @@ function generateCode(ast, options) {
   var heapSize   = exact.heap
   var entryPoint = exact.entry
 
+  var body = [ '\n\n\n\n/*Exact predicate*/', exact.code ]
+
+  if(!options.skipInterval) {
+    var interval = compileInterval(ast, options, entryPoint)
+    heapSize = Math.max(heapSize, interval.heap)
+    entryPoint = interval.entry
+    body.push(
+      '\n\n\n\n/*Interval predicate*/',
+      interval.code)
+  }
+
   var code = [
     'var heap_f64=new Float64Array(' + Math.ceil(heapSize/8) + ')',
     'var heap_u32=new Uint32Array(heap_f64.buffer)',
     'var heap_u16=new Uint16Array(heap_f64.buffer)',
     'var heap_u8=new Uint8AArray(heap_f64.buffer)',
-    '\n\n\n\n',
-    '/*Exact predicate*/',
-    exact.code,
+    body.join('\n'),
     'return ' + entryPoint
   ]
 
